6cdce508319931e8ff46a4f21ab07564017f3117
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-02-26 00:39:44 -0800

    TypeScript - problem 8

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -6,6 +6,7 @@ const solutions = [
   { problem: 5, solution: "232792560" },
   { problem: 6, solution: "25164150" },
   { problem: 7, solution: "104743" },
+  { problem: 8, solution: "23514624000" },
 ];
 
 describe("Project Euler", () => {
