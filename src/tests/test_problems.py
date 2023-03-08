21c094754e76e1d2f6440716322cea5fe94a5fc3
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-03-08 15:22:52 -0800

    Use pytest instead of unittest

---
+++ src/tests/test_problems.py
@@ -0,0 +1,20 @@
+import importlib
+import pytest
+
+SOLUTIONS = [
+    {"problem": 1, "solution": "233168"},
+    {"problem": 2, "solution": "4613732"},
+]
+
+
+@pytest.mark.parametrize(
+    ["problem", "solution"],
+    [(item["problem"], item["solution"]) for item in SOLUTIONS],
+    ids=[f'problem {item["problem"]}' for item in SOLUTIONS],
+)
+def test_problems(problem: int, solution: str) -> None:
+    m = importlib.import_module(  # pylint: disable=invalid-name
+        f"src.{problem:03d}.p{problem:03d}"
+    )
+    result = str(m.solve())
+    assert result == solution
