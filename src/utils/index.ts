94f6798b32a432621e2b43ef1c103fcd000ef7a8
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-03-16 11:48:09 -0700

    Refactor utils directory

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,1 @@
-export { arrayProduct, arraySum } from "./array";
-export { digitSum } from "./digitSum";
-export { countOfDivisors, divisors, sumOfDivisors } from "./divisors";
-export { factorial } from "./factorial";
-export { fibonacci } from "./fibonacci";
-export { gcd } from "./gcd";
-export { lcm } from "./lcm";
-export { divide, factorize, primes } from "./primeNumbers";
-export { range } from "./range";
+export * from "./ts";
---
+++ src/utils/ts/index.ts
@@ -0,0 +1,9 @@
+export { arrayProduct, arraySum } from "./array";
+export { digitSum } from "./digitSum";
+export { countOfDivisors, divisors, sumOfDivisors } from "./divisors";
+export { factorial } from "./factorial";
+export { fibonacci } from "./fibonacci";
+export { gcd } from "./gcd";
+export { lcm } from "./lcm";
+export { divide, factorize, primes } from "./primeNumbers";
+export { range } from "./range";
