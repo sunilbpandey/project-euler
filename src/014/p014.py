d63545b7e12fccc3d98aad68b5aead984e8c66cc
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-03-22 15:22:57 -0700

    Python - some optimizations to the solution for problem 14

--- src/014/p014.py
+++ src/014/p014.py
@@ -1,22 +1,24 @@
 def solve() -> int:
-    chain_lengths = {1: 1}
+    chain_lengths = [0] * 1000000
+    chain_lengths[1] = 1
 
     max_chain_length = 0
     max_chain_length_start = 0
 
-    for start in range(2, 1000000):
+    for start in range(500000, 1000000):
         chain_length = 0
         num = start
-        while num not in chain_lengths:
+        while True:
             if num % 2 == 0:
                 num = num // 2
+                if num < start:
+                    chain_lengths[start] = chain_length + chain_lengths[num]
+                    break
             else:
                 num = 3 * num + 1
             chain_length += 1
-        chain_length += chain_lengths[num]
-        chain_lengths[start] = chain_length
 
-        if chain_length > max_chain_length:
-            max_chain_length = chain_length
+        if chain_lengths[start] > max_chain_length:
+            max_chain_length = chain_lengths[start]
             max_chain_length_start = start
     return max_chain_length_start
