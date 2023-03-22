9d297596a854af12f0ab44cd2f6b3ba2d710d60e
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-03-22 15:06:03 -0700

    Python - problem 14

---
+++ src/014/p014.py
@@ -0,0 +1,22 @@
+def solve() -> int:
+    chain_lengths = {1: 1}
+
+    max_chain_length = 0
+    max_chain_length_start = 0
+
+    for start in range(2, 1000000):
+        chain_length = 0
+        num = start
+        while num not in chain_lengths:
+            if num % 2 == 0:
+                num = num // 2
+            else:
+                num = 3 * num + 1
+            chain_length += 1
+        chain_length += chain_lengths[num]
+        chain_lengths[start] = chain_length
+
+        if chain_length > max_chain_length:
+            max_chain_length = chain_length
+            max_chain_length_start = start
+    return max_chain_length_start
