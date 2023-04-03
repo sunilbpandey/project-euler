ce1f613e8a42e55f9c1c614ae49d45f5cb324b2c
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-04-03 12:37:01 -0700

    Python - alternative solution for problem 45

--- src/045/p045.py
+++ src/045/p045.py
@@ -1,5 +1,5 @@
 import math
-from src.utils.py.series import triangle_numbers
+from src.utils.py.series import pentagonal_numbers, triangle_numbers
 
 
 def is_pentagonal_number(number: int) -> bool:
@@ -12,8 +12,15 @@ def is_hexagonal_number(number: int) -> bool:
     return root % 1 == 0 and (int(root) + 1) % 4 == 0
 
 
-def solve() -> int:
+def solve1() -> int:
     for num in triangle_numbers():
         if num > 40755 and is_pentagonal_number(num) and is_hexagonal_number(num):
+            return num
+    raise RuntimeError("No solution found")
+
+
+def solve() -> int:
+    for num in pentagonal_numbers():
+        if num > 40755 and is_hexagonal_number(num):
             return num
     raise RuntimeError("No solution found")
--- src/045/solution.md
+++ src/045/solution.md
@@ -28,3 +28,13 @@ $$
 In other words, for a number, $h$, to be hexagonal, $8h + 1$ must be a perfect square.
 
 So we can generate triangular numbers and use these two tests to check if the number is also pentagonal and hexagonal.
+
+However, we can do one small optimization.
+
+Consider the triangle number,
+
+$$
+T_{2n - 1} = \frac{(2n - 1)(2n)}{2} = n(2n - 1) = H_n
+$$
+
+So, every hexagonal number is also a triangle number. This means if we just generate pentagonal numbers and test if the number is also hexagonal, we can get the answer without having to test if the number is triangular.
