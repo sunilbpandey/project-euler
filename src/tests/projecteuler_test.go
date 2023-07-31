8244e19a9dbcd4715a1d2ef4d4da117949779139
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-07-31 11:53:05 -0700

    Minor refactoring

--- src/tests/projecteuler_test.go
+++ src/tests/projecteuler_test.go
@@ -5,12 +5,6 @@ import (
 	"fmt"
 	"os"
 	"testing"
-
-	"github.com/sunilbpandey/project-euler/src/001"
-	"github.com/sunilbpandey/project-euler/src/002"
-	"github.com/sunilbpandey/project-euler/src/003"
-	"github.com/sunilbpandey/project-euler/src/004"
-	"github.com/sunilbpandey/project-euler/src/005"
 )
 
 type Record struct {
@@ -36,16 +30,6 @@ func ReadAnswers() map[int]string {
 		answers[record.Problem] = record.Answer
 	}
 	return answers
-}
-
-type ProblemFunc func() string
-
-var problemFuncs = map[int]ProblemFunc{
-	1: problem001.Solve,
-	2: problem002.Solve,
-	3: problem003.Solve,
-	4: problem004.Solve,
-	5: problem005.Solve,
 }
 
 func TestProblems(t *testing.T) {
