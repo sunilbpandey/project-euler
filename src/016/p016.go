054ca615335f3bd1a9069738b272c0779bd466eb
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-08-03 12:42:40 -0700

    Go - move common code to utils

--- src/016/p016.go
+++ src/016/p016.go
@@ -3,13 +3,12 @@ package problem016
 import (
 	"math/big"
 	"strconv"
+
+	"github.com/sunilbpandey/project-euler/src/utils/go/bigintmath"
 )
 
 func Solve() string {
 	n := big.NewInt(0).Exp(big.NewInt(2), big.NewInt(1000), nil)
-	sum := 0
-	for _, digit := range n.String() {
-		sum += int(digit - '0')
-	}
+	sum := bigintmath.DigitSum(n)
 	return strconv.Itoa(sum)
 }
--- src/utils/go/bigintmath/math.go
+++ src/utils/go/bigintmath/math.go
@@ -2,6 +2,14 @@ package bigintmath
 
 import "math/big"
 
+func DigitSum(n *big.Int) int {
+	sum := 0
+	for _, digit := range n.String() {
+		sum += int(digit - '0')
+	}
+	return sum
+}
+
 func Factorial(n int) *big.Int {
 	factorial := big.NewInt(1)
 	for m := 2; m <= n; m++ {
