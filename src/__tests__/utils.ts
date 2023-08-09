daf9d0904bb4ce5bb5b05a279b631c3023467133
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-08-09 10:57:31 -0700

    Refactor TypeScript tests

--- src/__tests__/utils.ts
+++
@@ -1,5 +0,0 @@
-export const solve = async (problem: number): Promise<string> => {
-  const str = problem.toString().padStart(3, "0");
-  const { solve } = await import(`../${str}/p${str}`);
-  return solve();
-};
---
+++ src/tests/projecteuler.test.ts
@@ -0,0 +1,12 @@
+import answers from "../answers.json";
+
+describe("Project Euler", () => {
+  test.each(answers.filter(({ problem }) => problem <= 25))(
+    "Problem $problem",
+    async ({ problem, answer }) => {
+      const str = problem.toString().padStart(3, "0");
+      const { solve } = await import(`../${str}/p${str}`);
+      expect(await solve()).toBe(answer);
+    }
+  );
+});
