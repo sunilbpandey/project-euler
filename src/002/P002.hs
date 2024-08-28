b3dfa680102fa64d4e3aad471203f36448bf2086
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-08-28 00:36:38 -0700

    Haskell - problem 2

---
+++ src/002/P002.hs
@@ -0,0 +1,33 @@
+module P002 where
+
+-- Fibonacci series implemented as an infinite list
+fibonacci :: [Int]
+fibonacci = 1:1:zipWith (+) fibonacci (tail fibonacci)
+
+-- Sum every even number less than 4000000
+solution' :: Int
+solution' = sum . takeWhile (< 4000000) . filter even . tail $ fibonacci
+
+every :: Int -> [a] -> [a]
+every n xs = case drop (n-1) xs of [] -> []
+                                   (y:ys) -> y:every n ys
+
+-- Sum every third number less than 4000000
+solution'' :: Int
+solution'' = sum . takeWhile (< 4000000) . every 3 $ fibonacci
+
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ [] = []
+takeUntil f (x:xs) = if f x then [x] else x:takeUntil f xs
+
+lastN :: Int -> [a] -> [a]
+lastN n xs = drop (length xs - n) xs
+
+-- Use the largest term less than 4000000 and the next term to calculate the answer
+solution :: Int
+solution = div (s - 1) 2
+    where
+        [a,b] = lastN 2 . takeUntil (> 4000000) $ fibonacci
+        s | even b    = a
+          | odd a     = b
+          | otherwise = a + b
