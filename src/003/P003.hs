7a35645b3b7016ffca3d1558fdb58f573f17002f
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-08-28 22:24:28 -0700

    Haskell - problem 3

---
+++ src/003/P003.hs
@@ -0,0 +1,26 @@
+module P003 where
+
+import Data.List (group)
+
+sqrtFloor :: Integer -> Integer
+sqrtFloor = floor . sqrt . fromIntegral
+
+isPrime :: Integral a => a -> [a] -> Bool
+isPrime _ [] = True
+isPrime n (p:ps) = mod n p /= 0 && isPrime n ps
+
+primes :: [Integer]
+primes = 2:[n|n <- [3,5..], isPrime n (takeWhile (<= sqrtFloor n) primes)]
+
+factorize :: Integer -> [(Integer, Int)]
+factorize n =
+    map (\l@(x:xs) -> (x, length l)) . group . factors n $ takeWhile (<= sqrtFloor n) primes
+    where
+        factors m [] = [m]
+        factors m (p:ps)
+            | p > m        = []
+            | mod m p == 0 = p:factors (div m p) (p:ps)
+            | otherwise    = factors m ps
+
+solution :: Int
+solution = fromIntegral . fst . last . factorize $ 600851475143
