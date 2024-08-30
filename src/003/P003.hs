de488079ee43401b5a966e0402bad0abc1592eb4
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-08-30 07:55:39 -0700

    Haskell - alternative solution for problem 3

--- src/003/P003.hs
+++ src/003/P003.hs
@@ -22,8 +22,23 @@ factorize n =
             | mod m p == 0 = p:factors (div m p) (p:ps)
             | otherwise    = factors m ps
 
+solution' :: Int
+solution' = fromIntegral . fst . last . factorize $ 600851475143
+
+-- Alternative solution, without calculating primes or keeping a list of factors
+largestFactor :: Int -> Int
+largestFactor 1 = 1
+largestFactor n
+    | even n    = largestFactor (div n 2)
+    | otherwise = max 2 $ largestFactor' n 3
+    where
+        largestFactor' m d
+            | d*d > m      = m
+            | mod m d == 0 = max d $ largestFactor' (div m d) d
+            | otherwise    = largestFactor' m (d+2)
+
 solution :: Int
-solution = fromIntegral . fst . last . factorize $ 600851475143
+solution = largestFactor 600851475143
 
 main :: IO ()
 main = print solution
