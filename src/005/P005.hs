1b3a47aa74eaaa8473ab1634d99c96afb2c2bb81
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-09-05 14:08:36 -0700

    Haskell - problem 5

---
+++ src/005/P005.hs
@@ -0,0 +1,10 @@
+module P005 where
+
+solution :: Integer
+solution = foldl lcm' 1 [2..20]
+    where
+        gcd' a b = if b == 0 then a else gcd' b (mod a b)
+        lcm' a b = div (a * b) (gcd' a b)
+
+main :: IO ()
+main = print solution
