64fad04249b75032220705f67f323cfcf486acaa
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-09-06 18:42:26 -0700

    Haskell - problem 7

---
+++ src/007/P007.hs
@@ -0,0 +1,17 @@
+module P007 where
+
+sqrtFloor :: Integer -> Integer
+sqrtFloor = floor . sqrt . fromIntegral
+
+isPrime :: Integral a => a -> [a] -> Bool
+isPrime _ [] = True
+isPrime n (p:ps) = mod n p /= 0 && isPrime n ps
+
+primes :: [Integer]
+primes = 2:[n|n <- [3,5..], isPrime n (takeWhile (<= sqrtFloor n) primes)]
+
+solution :: Integer
+solution = primes !! 10000
+
+main :: IO ()
+main = print solution
