a042a7a1a7d600bfd7dfc04f06d2bf51c42ebc79
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-09-17 21:33:17 -0700

    Haskell - refactor

--- src/P021/P021.hs
+++ src/P021/P021.hs
@@ -1,13 +1,6 @@
 module P021.P021 (solution) where
 
-import Data.List (nub, sort)
-import ProjectEuler.PrimeUtils (sqrtFloor)
-
-divisors :: Integer -> [Integer]
-divisors n = (sort . concat) [nub [d, div n d] | d <- [1..(sqrtFloor n)], mod n d == 0]
-
-properDivisorSum :: Integer -> Integer
-properDivisorSum = sum . init . divisors
+import ProjectEuler.Common (properDivisorSum)
 
 amicable :: Integer -> Bool
 amicable n = let m = properDivisorSum n in m /= n && n == properDivisorSum m
--- src/ProjectEuler/Common.hs
+++ src/ProjectEuler/Common.hs
@@ -1,9 +1,18 @@
 module ProjectEuler.Common where
 
 import Data.Char (digitToInt)
+import Data.List (nub, sort)
+
+import ProjectEuler.PrimeUtils (sqrtFloor)
 
 digitSum :: Show a => a -> Integer
 digitSum = toInteger . sum . (map digitToInt) . show
 
 fac :: Integer -> Integer
 fac = product . enumFromTo 1
+
+divisors :: Integer -> [Integer]
+divisors n = (sort . concat) [nub [d, div n d] | d <- [1..(sqrtFloor n)], mod n d == 0]
+
+properDivisorSum :: Integer -> Integer
+properDivisorSum = sum . init . divisors
